ab8635c4216029d5c08a5fb93bfbd2a992a438d6
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-01-21 01:07:57 +0200

    event- attribute- data- method sample

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
+
 const app=Vue.createApp({
     data(){
+        return{
+            title:"vue.js 1st day",
+            content:"hello world",
+            w3school:{
+                title:"learning   platform",
+                target:"_blank",
+                url:"https://www.w3schools.com",
+                alt:"the best learning platform",
+            },
+
+            workhard:"pogace",
+            coords:{
+                x:0,
+                y:0,
+
+            },
+        };
+    },
+    methods:{
+        changeTitle(pTitle) {
+     
+            this.title=pTitle;
         
-    }
-})
+        },
+        updateCoords(message, event){
+       this.changeTitle(`${event.x},${event.y}`);    
+       this.coords={
+        x:event.x,
+        y:event.y,
+       };
+
+        }, 
+    },
+    
+}).mount("#app");
